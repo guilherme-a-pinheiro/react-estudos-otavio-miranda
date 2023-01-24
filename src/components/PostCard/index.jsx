c0015ce90cf63fe5d19c9b04734d324f9bade9ab
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2023-01-24 11:23:37 -0300

    configurando button e acoes

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,10 +1,12 @@
+import './styles.css';
+
 export const PostCard = ({ title, body, id, cover }) => {
   // props são passadas no app principal e estou fazendo o desestructuring
   return (
     <div className="post">
       <img src={cover} alt={title} />
       <div className="post-content">
-        <h2>{title}</h2>
+        <h2>{title} {id}</h2>
         <p>{body}</p>
       </div>
     </div>
